0405d17c7dd4363e4ec6e2d03a055348ae38a1a3
muminutanov <muminutanov20@gmail.com>
2023-03-07 22:38:39 +0500

    05done

--- String05.py
+++ String05.py
@@ -7,4 +7,9 @@ def main(s1,s2):
     Returns:
         str: return answer.
     """
-    return 
+    s1="python"
+    s2="coder"
+    
+    return ("python " + "coder")
+x=main("python","coder")
+print(x)
